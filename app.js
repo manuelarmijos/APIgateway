ac73dce22f428e994860a09b73f89888bd30f9b2
manolo1994 <msarmijiso@gmail.com>
2023-04-12 23:32:34 -0500

    cors

--- app.js
+++ app.js
@@ -95,6 +95,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+
 // catch 404 and forward to error handler
 
 
