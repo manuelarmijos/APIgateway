e2b0a0daf2fcf53f94cc92359763a0bd071eaefe
manolo1994 <msarmijiso@gmail.com>
2023-04-13 02:44:46 -0500

    cambios

--- source/config/config.js
+++ source/config/config.js
@@ -38,9 +38,14 @@ exports.proxies = {
             //delete proxyRes.headers['x-removed'];       // remove header from response
         },
         onProxyReq(proxyReq, req, res) {
-            if (req.method == "POST" && req.body) {
-                proxyReq.write(encodeURIComponent(JSON.stringify(req.body)));
-                proxyReq.end();
+            console.log('si')
+            if (req.body) {
+                let bodyData = JSON.stringify(req.body);
+                // incase if content-type is application/x-www-form-urlencoded -> we need to change to application/json
+                proxyReq.setHeader('Content-Type', 'application/json');
+                proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
+                // stream the content
+                proxyReq.write(bodyData);
             }
         }
     },
